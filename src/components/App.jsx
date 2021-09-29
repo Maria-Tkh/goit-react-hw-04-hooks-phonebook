585edb03261e62cf3a8d18425c77849ea9e62cdc
Maria-Tkh <mcweaver117@gmail.com>
2021-09-29 16:09:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,38 +6,26 @@ import { Filter } from './Filter/Filter';
 import { PhonebookTitle, ContactTitle } from './App.styled';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState([
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ]);
+  const defaultValue = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
   
   const [filter, setFilter] = useState('');
 
-  const [contacts, setContacts] = useState(
-    JSON.parse(window.localStorage.getItem('contacts')) ?? '',
-  );
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem("contacts")) ?? defaultValue
+    );
+  });
  
-
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  //  useLocalStorage = (key, defaultValue) => {
-  //   const [state, setState] = useState(() => {
-  //     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-  //   });
-
-  //   useEffect(() => {
-  //     window.localStorage.setItem(key, JSON.stringify(state));
-  //   }, [key, state]);
-
-  //   return [state, setState];
-  // }
-
-
-   const formSubmitHandler = ( name, number ) => {
+     const formSubmitHandler = ( name, number ) => {
     const newContact = {
       id: uuidv4(),
       name,
